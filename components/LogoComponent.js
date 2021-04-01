09fd17badbf32800fd11d9749c6e55e8db6bb089
ar-rehman135 <abdulrehmanmirza135@gmail.com>
2021-04-01 17:37:18 +0500

    update logo

--- components/LogoComponent.js
+++ components/LogoComponent.js
@@ -13,7 +13,7 @@ export class Logo extends Component {
 			<View style={styles.container}>
         <Image
           style={{width: 100, height: 100}}
-          source={require('../assets/logo.png')}
+          source={require('../assets/logoo.jpg')}
         />
       <Text style={styles.logoText}>Welcome to Shop Online.</Text>
   		</View>
--- shared/ServerConf.js
+++ shared/ServerConf.js
@@ -1,1 +1,1 @@
-export const baseUrl = "http://192.168.8.102:5000/";
+export const baseUrl = "http://192.168.8.103:5000/";
